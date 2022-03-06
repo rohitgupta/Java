421eddad8e3705c38a37120e5bd8131234e0b583
rohit782192@gmail.com <Git@782192>
2022-03-06 12:46:01 +0530

    Pass

---
+++ src/com/alter/rohit/calculator/BloodPressure.java
@@ -0,0 +1,47 @@
+package com.alter.rohit.calculator;
+
+public class BloodPressure {
+    public static void main(String[] args) {
+        BloodPressure bp = new BloodPressure();
+        bp.PressureCheckDiastolic(120);
+        bp.PressureCheckSystolic(20);
+    }
+
+    public void PressureCheckSystolic(double Spressure) {
+
+        if (Spressure < 120) {
+            System.out.println("Enjoy ");
+            System.out.println("Systolic Blood Pressure  is Normal");
+        } else if (Spressure <= 120 && Spressure <= 139) {
+            System.out.println("Moderate Start Dieting.");
+            System.out.println("Systolic Blood Pressure at Risk (Pre Hypertension)");
+        } else if (Spressure >= 140) {
+            System.out.println("Start Taking Precaution.");
+            System.out.println("!!Systolic!!High Blood Pressure (Hypertension)!!!");
+        } else {
+            System.out.println("You are Normal");
+
+        }
+    }
+        public void PressureCheckDiastolic(double Dpressure){
+            if(Dpressure <80){
+                System.out.println("Enjoy ");
+                System.out.println("Diastolic Blood Pressure  is Normal");
+            }
+            else if(Dpressure == 80 && Dpressure == 89){
+                System.out.println("Start Taking Precaution.");
+                System.out.println("!!Diastolic!!High Blood Pressure (Hypertension)!!!");
+            }
+            else if (Dpressure >= 90){
+                System.out.println("Start Taking Precaution.");
+                System.out.println("!Diastolic!!!High Blood Pressure (Hypertension)!!!");
+            }
+            else{
+                System.out.println("Bhai you are Good Enjoy Life");
+
+            }
+
+        }
+
+
+}
