b3d67c91583eaf532fac4d7a138fbe3b4742477c
rohit782192@gmail.com <Git@782192>
2022-03-06 12:20:39 +0530

    Pass

---
+++ src/com/alter/rohit/calculator/CalculatorMethod.java
@@ -0,0 +1,38 @@
+package com.alter.rohit.calculator;
+
+public class CalculatorMethod {
+    public static void main(String[] args) {
+        CalculatorMethod cal = new CalculatorMethod();
+        System.out.println("The addition is "+cal.add(3,3));
+        System.out.println("The Multiplication is "+cal.Multiply(3,3));
+        System.out.println("The Division is "+cal.Division(4,4));
+        System.out.println("The Modulus is "+cal.Mod(5,7));
+        System.out.println("The Subtraction is :"+cal.Sub(5,8));
+    }
+
+    public int add(int x, int y){
+        int z = 0;
+         z =  x+y;
+        return z;
+    }
+
+    public int Sub(int x , int y){
+        int z = 0;
+        return z = x-y;
+    }
+
+    public int Multiply(int x, int y){
+        int z = 0;
+        return z = x*y;
+    }
+
+    public int Division(int x, int y){
+        int z = 0;
+        return z = x/y;
+    }
+
+    public int Mod(int x , int y){
+        int z = 0;
+        return z =  x%y;
+    }
+}
--- src/com/alter/rohit/calculator/MethodExampleJava.java
+++ src/com/alter/rohit/calculator/MethodExampleJava.java
@@ -2,16 +2,23 @@ package com.alter.rohit.calculator;
 
 public class MethodExampleJava {
 
-    public void Name(String fname){
+    public void Name(String fname, int age){
         System.out.println("The Name is :"+fname);
+        System.out.println("The age is "+age);
     }
+
+    public int add(int x){
+        return 5+x;
+    }
+
 
     public static void main(String[] args) {
         MethodExampleJava method = new MethodExampleJava();
-        method.Name("Rohit");
-        method.Name("Vikas");
-        method.Name("Shukla");
-        method.Name("Dongry");
+        method.Name("Rohit",44);
+        method.Name("Vikas",55);
+        method.Name("Shukla",33);
+        method.Name("Dongry",44);
+        System.out.println("The total is "+method.add(6));
     }
 
 }
